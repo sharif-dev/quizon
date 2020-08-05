08c780c81681b3f90a81b7ce0dd2c6a99257d8f1
Allarious <alirezaicq4@gmail.com>
2020-08-05 23:49:54 +0430

    finished quizon

--- quizon/WelcomeViewController.swift
+++ quizon/WelcomeViewController.swift
@@ -43,25 +43,25 @@ class WelcomeViewController: UIViewController {
             Question(question: "How many planets are there in The Solar System?", answers: ["5", "6", "7", "8"], correct: 4),
             Question(question: "Which one is the radius of earth?", answers: ["6371", "5634", "2654", "9868"], correct: 1),
             Question(question: "What is the distance between earth and sun called?", answers: [ "1KM", "1AU", "1MI", "1DN"], correct: 2),
-            Question(question: "Astronomy4", answers: ["A", "B", "C", "D"], correct: 2),
-            Question(question: "Astronomy5", answers: ["A", "B", "C", "D"], correct: 2),
+            Question(question: "Which of these planets is closer to the Sun than Earth?", answers: ["Jupiter", "Saturn", "Venus", "Mercury"], correct: 3),
+            Question(question: "How many days does it take for Earth to travel around the Sun?", answers: ["415", "225", "365", "743"], correct: 3)
             ])
         case "Maths":
             quizController.quiz = Quiz(questions: [
-            Question(question: "Maths1", answers: ["L", "M", "N", "O"], correct: 2),
-            Question(question: "Maths2", answers: ["L", "M", "N", "O"], correct: 2),
-            Question(question: "Maths3", answers: ["L", "M", "N", "O"], correct: 2),
-            Question(question: "Maths4", answers: ["L", "M", "N", "O"], correct: 2),
-            Question(question: "Maths5", answers: ["L", "M", "N", "O"], correct: 2),
+            Question(question: "Whats is the result of 2 + 2 = ?", answers: ["3", "4", "5", "6"], correct: 2),
+            Question(question: "What is the result of 4 x 5 = ?", answers: ["10", "15", "20", "25"], correct: 3),
+            Question(question: "If any number is multiply with 0 then what will be answered?", answers: ["0", "1", "2", "The number itself"], correct: 1),
+            Question(question: "What 2D shape has 3 sides", answers: ["Square", "Rectangle", "Circle", "Triangle"], correct: 4),
+            Question(question: "What is the smallest two digit number?", answers: ["99", "80", "10", "20"], correct: 3),
             ])
 
         case "Physics":
             quizController.quiz = Quiz(questions: [
-            Question(question: "Physics1", answers: ["P", "Q", "R", "S"], correct: 2),
-            Question(question: "Physics2", answers: ["P", "Q", "R", "S"], correct: 2),
-            Question(question: "Physics3", answers: ["P", "Q", "R", "S"], correct: 2),
-            Question(question: "Physics4", answers: ["P", "Q", "R", "S"], correct: 2),
-            Question(question: "Physics5", answers: ["P", "Q", "R", "S"], correct: 2),
+            Question(question: "What is an ohm measure?", answers: ["air resistance", "electrical resistance", "temperature of plasma", "volume of gas"], correct: 2),
+            Question(question: "Which of these is NOT a state of matter?", answers: ["gas", "liquid", "solid", "they are all states of matter"], correct: 4),
+            Question(question: "The ability of a substance to return to its shape after being deformed is called ______.", answers: ["elasticity", "flexibility", "metaflex", "integrity"], correct: 1),
+            Question(question: "In nuclear physics, what is the opposite of fusion?", answers: ["stasis", "fission", "partition", "replication"], correct: 2),
+            Question(question: "The upward force that water exerts on objects is called ________.", answers: ["buoyancy", "impermeability", "resistance", "water pressure"], correct: 1),
             ])
 
         default:
